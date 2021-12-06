c98bd4bc2a1c42d0a1888601cefcf415acb152c1
Slider <cph-yk31@cphbusiness.dk>
2021-12-06 19:44:39 +0100

    add user and new droplet (smooth)

--- src/components/Urls.jsx
+++ src/components/Urls.jsx
@@ -1,6 +1,8 @@
 //const Server_URL = "https://manlyman69.rocks/CA3";
 
-const Server_URL = "http://localhost:8080/CA3";
+const Server_URL = "https://shitlord.me/CA3/";
+
+//const Server_URL = "http://localhost:8080/CA3";
 
 const MovieTitle = Server_URL + "/api/movie/title/";
 
